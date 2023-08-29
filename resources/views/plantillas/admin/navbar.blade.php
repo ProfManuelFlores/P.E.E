b2761d9445c8d10bb1292c08a8e85cfe7f917184
gloverProgramando <gloverotaku@outlook.es>
2023-08-29 12:13:09 -0500

    finish navbar

--- resources/views/plantillas/admin/navbar.blade.php
+++ resources/views/plantillas/admin/navbar.blade.php
@@ -14,26 +14,26 @@
         </button>
         <div class="navbar_content_links" id="navbar-dropdown">
             <ul class="navbar_content_links_normal">
-                <li>
+                <li class="navbar_content_links_contents">
                     <a href="#">Inicio</a>
                 </li>
-                <li>
+                <li class="navbar_content_links_contents">
                     <a href="#">Registros</a>
                 </li>
-                <li>
+                <li class="navbar_content_links_contents">
                     <a href="#">Usuarios</a>
                 </li>
                 <li>
                     <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-                        class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Registros
+                        class="navbar_dropwdown">Registros
                         <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
-                    <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                    <div id="dropdownNavbar" class="navbar_content_dropdown">
+                        <ul class="text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
                             <li>
                                 <a href="">Estancias I</a>
                             </li>
@@ -52,7 +52,7 @@
                         </ul>
                     </div>
                 </li>
-                <li>
+                <li class="navbar_content_links_contents">
                     <a href="#">Cerrar Sesion</a>
                 </li>
             </ul>
--- resources/views/users/admin/inicio-admin.blade.php
+++ resources/views/users/admin/inicio-admin.blade.php
@@ -15,7 +15,19 @@
 
     </header>
     <section>
-        
+        <div class="home_admin">
+            <div class="home_top_admin">
+                <p class="title">Bienvenido: [nombre de usuario]</p>
+            </div>
+            <div class="home_botton_admin">
+                <div class="home_botton_admin_present">
+                    <p class="subtitle"> Periodo Actual </p>
+                </div>
+                <div class="home_botton_admin_general">
+                    <p class="subtitle"> General </p>
+                </div>
+            </div>
+        </div>
     </section>
     @include('plantillas.commun.footer')
 </body>
