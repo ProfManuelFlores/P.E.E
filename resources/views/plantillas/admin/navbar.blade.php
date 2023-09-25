78746435ef1847b59bcbf90446518ae948d3cb68
gloverProgramando <gloverotaku@outlook.es>
2023-09-25 11:51:06 -0500

    fixed 3

--- resources/views/plantillas/admin/navbar.blade.php
+++ resources/views/plantillas/admin/navbar.blade.php
@@ -3,8 +3,7 @@
         <a href="#" class="flex items-center">
             <img src="{{ asset('assets/images/logosinfondo.png') }}" class="mr-1 md:h-20 h-14" alt="Upqroo Logo" />
         </a>
-        <button data-collapse-toggle="navbar-default" type="button"
-            class="navbar_Admin_button"
+        <button data-collapse-toggle="navbar-default" type="button" class="navbar_Admin_button"
             aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -14,27 +13,29 @@
             </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul
-                class="navbar_content_links_contents">
+            <ul class="navbar_content_links_contents">
                 <li>
-                    <a href="#"
-                        class="">Home</a>
+                    <a href="#" class="navbar_content_links_pages">Usuarios</a>
                 </li>
                 <li>
-                    <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                    <a href="#" class="navbar_content_links_pages">Registros</a>
                 </li>
                 <li>
-                    <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                    <a href="#" class="navbar_content_links_pages">Formatos</a>
                 </li>
                 <li>
-                    <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                    <a href="#" class="navbar_content_links_pages">Periodos</a>
                 </li>
                 <li>
-                    <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    <a class="navbar_content_links_pages" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                        {{ __('Cerrar Sesion') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </li>
             </ul>
         </div>
