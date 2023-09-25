c0dc4caf4fa31bf02e6e7683adb5d2c3bb410b25
gloverProgramando <gloverotaku@outlook.es>
2023-09-25 11:33:48 -0500

    fixing navbar

--- resources/views/plantillas/admin/navbar.blade.php
+++ resources/views/plantillas/admin/navbar.blade.php
@@ -1,10 +1,11 @@
 <nav class="navbar__Admin">
     <div class="navbar_content_admin">
-        <div class="navbar__Admin__holder">
-            <img src="{{ asset('assets/images/logosinfondo.png') }}" alt="Logo Universidad Politecnica De Quintana Roo">
-        </div>
-        <button data-collapse-toggle="navbar-dropdown" type="button" class="navbar_Admin_button"
-            aria-controls="navbar-dropdown" aria-expanded="false">
+        <a href="#" class="flex items-center">
+            <img src="{{ asset('assets/images/logosinfondo.png') }}" class="mr-1 md:h-20 h-14" alt="Upqroo Logo" />
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button"
+            class="navbar_Admin_button"
+            aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 17 14">
@@ -12,56 +13,29 @@
                     d="M1 1h15M1 7h15M1 13h15" />
             </svg>
         </button>
-        <div class="navbar_content_links" id="navbar-dropdown">
-            <ul class="navbar_content_links_normal">
-                <li class="navbar_content_links_contents">
-                    <a href="#">Inicio</a>
-                </li>
-                <li class="navbar_content_links_contents">
-                    <a href="#">Registros</a>
-                </li>
-                <li class="navbar_content_links_contents">
-                    <a href="#">Usuarios</a>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul
+                class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#"
+                        class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                        aria-current="page">Home</a>
                 </li>
                 <li>
-                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-                        class="navbar_dropwdown">Registros
-                        <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
-                    <div id="dropdownNavbar" class="navbar_content_dropdown">
-                        <ul class="text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                            <li>
-                                <a href="">Estancias I</a>
-                            </li>
-                            <li>
-                                <a href="">Estancias II</a>
-                            </li>
-                            <li>
-                                <a href="">Estadias</a>
-                            </li>
-                            <li>
-                                <a href="">Estadias Nacionales</a>
-                            </li>
-                            <li>
-                                <a href="">Servicio Social</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <a href="#"
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
                 </li>
-                <li class="navbar_content_links_contents">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                        {{ __('Cerrar Sesion') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
+                <li>
+                    <a href="#"
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                 </li>
             </ul>
         </div>
